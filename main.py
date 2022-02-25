69f359d69f5d64cf5c87ea24892e9ac0e4810eff
Simon Tulling <simon.tulling99@gmail.com>
2022-02-25 11:54:40 +0100

    everysing

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+url = 'http://192.168.1.120:5000/'
+data = [
+    "wave",
+    "sparkle",
+    "mond",
+    "fill",
+    "diamond",
+    "circle",
+    "bars",
+    "clear",
+    "snake"
+]
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", url=url, data=data)
+
+app.run(host='0.0.0.0', port=80)
