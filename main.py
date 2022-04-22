058fc8680677dafe5489d45b1168175f5c1cfb79
AaronvDiepen <aaronvdiepen@gmail.com>
2022-04-22 21:32:15 +0200

    Add requirements.txt and Minor layout changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sys import argv
 from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_cors import CORS
 
@@ -14,11 +15,16 @@ data = [
     "circle",
     "bars",
     "clear",
-    "snake"
+    "snake",
+    "clock",
 ]
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", url=url, data=data)
 
-app.run(host='0.0.0.0', port=80)
+port = 80
+if len(argv) > 1:
+    port = argv[1]
+
+app.run(host='0.0.0.0', port=port)
