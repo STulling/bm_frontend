f71d2fd0d83d094359de90446f4f02fe0c7da5d9
AaronvDiepen <aaronvdiepen@gmail.com>
2022-04-22 22:57:32 +0200

    Add cleaner animation for button presses

--- static/js.js
+++ static/js.js
@@ -1,7 +1,7 @@
-function sendreq(theUrl)
-{
+function sendreq(theUrl) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
+    xmlHttp.open("GET", theUrl, true ); // false for synchronous request
+    xmlHttp.send(null);
+    console.log("test");
     return xmlHttp.responseText;
 }
